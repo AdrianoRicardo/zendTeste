c41d28c9a02f8cf97a01f3571b0febc25290fc72
Adriano <Adriano@adriano>
2014-12-31 08:39:37 -0200

    Primeiro Commit - Teste

--- module/Application/src/Application/Controller/UsuarioController.php
+++ module/Application/src/Application/Controller/UsuarioController.php
@@ -6,6 +6,10 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
 class UsuarioController extends AbstractActionController{
-    
+    /**/
+    public function cadastrar(){
+        
+        echo 'coisa';
+    }
     
 }
